3b94c26160413744bb41d788675bc8a951fda555
Voya <voya@voyacode.com>
2016-12-28 18:43:35 +0200

    Add storage to settings

--- src/services/settings.service.ts
+++ src/services/settings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
+import { Storage } from '@ionic/storage'
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
@@ -12,20 +13,50 @@ export class SettingsService {
   max_guesses = 9;
   difficulty = 1;
 
-  constructor(private http: Http) { 
+  initialized = false;
+
+  // Supported languages
+  readonly languages:string[] = ['english'];
+
+  constructor(private http: Http, private storage: Storage) {
+  }
+
+  // Initialises settings
+  init(){
+    return this.storage.get('language').then((lang) => {
+      if(lang !== null){
+        this.language = lang;
+      }
+      console.log("Setting test:", lang);
+    }).then(() => {
+      return this.getLanguageFiles();
+    }).then(() => {
+        return this.storage.get('difficulty').then((value) => {
+          if(value !== null){
+            this.difficulty = value;
+          }
+      })
+    }).then(() => {
+      this.initialized = true;
+    })    
+  }
+
+  getLanguageFiles(){
+    return Promise.all([
+      this.http.get('lang/' + this.language + '.json').toPromise().then((data) =>{
+        this.lang = data.json();
+      }),
+      this.http.get('lang/word-lists/' + this.language + '-words.json').toPromise().then((data) =>{
+        this.wordList = data.json();
+      })
+    ]);
   }
 
   // Updates language setting
   updateLanguage(lang: string): Promise<any>{
     this.language = lang;
-
-    return Promise.all([
-      this.http.get('../lang/' + lang + '.json').toPromise().then((data) =>{
-        this.lang = data.json();
-      }),
-      this.http.get('../lang/word-lists/' + lang + '-words.json').toPromise().then((data) =>{
-        this.wordList = data.json();
-      })
-    ]);
+    return this.storage.set('language', lang).then(() => {
+      return this.getLanguageFiles();
+    });
   }
 }
