5e07b3e71df4213e1e13c9cab47b0f1248a60778
Voya <voya@voyacode.com>
2016-12-31 21:17:57 +0200

    Remove unnecessary import

--- src/services/settings.service.ts
+++ src/services/settings.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Storage } from '@ionic/storage'
 import 'rxjs/add/operator/toPromise';
