738a4b569dad46445c966e0eaa4a91f49d659c14
Voya <voya@voyacode.com>
2016-12-31 18:05:29 +0200

    Add back button functionality

--- src/shared/navbar/navbar.component.ts
+++ src/shared/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { App, NavController, Platform } from 'ionic-angular';
 
 import { HangmanPage } from '../../pages/hangman/hangman'
 import { SettingsPage } from '../../pages/settings/settings';
@@ -12,10 +12,29 @@ import { SettingsService } from '../../services/settings.service'
   templateUrl: 'navbar.component.html'
 })
 export class NavBarComponent implements OnInit {
-  constructor(public navCtrl: NavController,
+  constructor(private app: App,
+              public navCtrl: NavController,
+              private platform: Platform,
               private settings: SettingsService) { }
 
-  ngOnInit() { }
+  ngOnInit() { 
+    // Adds back button functionality to Android.
+    // All pages bring back to home page
+    // Note: This most likely isn't the best place to implement this functionality, because it is reset every time a page is switched.
+    // app.component.ts would likely be the best place, but it can't access NavControl, which is needed for this.
+    // If you know a better place to place this (in this kind of app structure), feel free to tell me. :)
+     
+    this.platform.registerBackButtonAction(() => {
+      const portal = this.app._appRoot._getPortal();
+      // If there are overlay views (language selection, etc.), pop them
+      if(portal.length() > 0){
+          portal.pop();
+      }else if(!(this.navCtrl.getActive().instance instanceof HangmanPage)){
+        this.navCtrl.setRoot(HangmanPage);
+      }
+    });
+  }
+  
 
   changePage(page){
     if(page == 'Hangman'){
